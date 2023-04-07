f6325fb1effd95d84669724067cc8e3576029a45
Maz Codes <marylandxox@gmail.com>
2023-04-07 16:33:05 +0100

    all add

--- Main2.m
+++ Main2.m
@@ -15,16 +15,16 @@ load('Data/dataset_MNIST.mat')
 
 %% Plot the 9 first images of X_train
 
-figure,
-subplot 331, imagesc(X_train(:,:,1)), axis image, axis off, colormap gray
-subplot 332, imagesc(X_train(:,:,2)), axis image, axis off, colormap gray
-subplot 333, imagesc(X_train(:,:,3)), axis image, axis off, colormap gray
-subplot 334, imagesc(X_train(:,:,4)), axis image, axis off, colormap gray
-subplot 335, imagesc(X_train(:,:,5)), axis image, axis off, colormap gray
-subplot 336, imagesc(X_train(:,:,6)), axis image, axis off, colormap gray
-subplot 337, imagesc(X_train(:,:,7)), axis image, axis off, colormap gray
-subplot 338, imagesc(X_train(:,:,8)), axis image, axis off, colormap gray
-subplot 339, imagesc(X_train(:,:,9)), axis image, axis off, colormap gray
+% figure,
+% subplot 331, imagesc(X_train(:,:,1)), axis image, axis off, colormap gray
+% subplot 332, imagesc(X_train(:,:,2)), axis image, axis off, colormap gray
+% subplot 333, imagesc(X_train(:,:,3)), axis image, axis off, colormap gray
+% subplot 334, imagesc(X_train(:,:,4)), axis image, axis off, colormap gray
+% subplot 335, imagesc(X_train(:,:,5)), axis image, axis off, colormap gray
+% subplot 336, imagesc(X_train(:,:,6)), axis image, axis off, colormap gray
+% subplot 337, imagesc(X_train(:,:,7)), axis image, axis off, colormap gray
+% subplot 338, imagesc(X_train(:,:,8)), axis image, axis off, colormap gray
+% subplot 339, imagesc(X_train(:,:,9)), axis image, axis off, colormap gray
 
 %% Useful dimensions
 
@@ -75,9 +75,9 @@ p= 0.001  ;
 theta = 0.8 ;
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-disp('Run stochastic prox gradient descent with')
-disp(['p = ',num2str(p)])
-disp(['theta = ',num2str(theta)])
+% disp('Run stochastic prox gradient descent with')
+% disp(['p = ',num2str(p)])
+% disp(['theta = ',num2str(theta)])
 [w_S, perc_error_S, crit_S, time_S] = ...
 FB_sto(winit, X_train_mat, Y_train, lambda, delta, p, theta, ...
             d_test, Y_test, ItMax, Stop_norm, Stop_crit) ;
@@ -86,14 +86,15 @@ FB_sto(winit, X_train_mat, Y_train, lambda, delta, p, theta, ...
 
 %% Show results
 
-figure, 
-subplot 121, plot(cumsum(time_S), perc_error_S), xlabel('time (s.)'), ylabel('error (%)'), axis([0 sum(time_S) 0.5*min(perc_error_S) 100])
-subplot 122, semilogy(cumsum(time_S), perc_error_S), xlabel('time (s.)'), ylabel('error (%)'), axis([0 sum(time_S) 0.5*min(perc_error_S) 100])
+% figure, 
+% subplot 121, plot(cumsum(time_S), perc_error_S), xlabel('time (s.)'), ylabel('error (%)'), axis([0 sum(time_S) 0.5*min(perc_error_S) 100])
+% subplot 122, semilogy(cumsum(time_S), perc_error_S), xlabel('time (s.)'), ylabel('error (%)'), axis([0 sum(time_S) 0.5*min(perc_error_S) 100])
 
-Lab_S = d_test(w_S) ;
-figure
-for i = 1:16
-    subplot(4,4,i), imagesc(X_test(:,:,i)), axis image, colormap gray
-    xlabel(['Label: true=',num2str(Y_test(i)),' vs. predicted=',num2str(Lab_S(i))])
-end
+% Lab_S = d_test(w_S) ;
+% figure
+% for i = 1:16
+%     subplot(4,4,i), imagesc(X_test(:,:,i)), axis image, colormap gray
+%     xlabel(['Label: true=',num2str(Y_test(i)),' vs. predicted=',num2str(Lab_S(i))])
+% end
 
+save("results")
